e68c19f34abef970c1e3fd7e4d015063311ccefe
chen9 <chen940226@gmail.com>
2021-03-16 01:51:53 +0800

    test

--- src/main/java/com/tiehca/apitest/heshang/ServletInitializer.java
+++ src/main/java/com/tiehca/apitest/heshang/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(HeshangApplication.class);
+        return application.sources(ShangGuiGuApplication.class);
     }
 
 }
---
+++ src/main/java/com/tiehca/apitest/heshang/ShangGuiGuApplication.java
@@ -0,0 +1,14 @@
+package com.tiehca.apitest.heshang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class ShangGuiGuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShangGuiGuApplication.class, args);
+    }
+
+}
