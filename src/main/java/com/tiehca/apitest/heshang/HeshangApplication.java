729002c35bdd0040857de7ced1fa8289048f3599
chen <chen940226@gmail.com>
2021-03-16 01:00:57 +0800

    project init

---
+++ src/main/java/com/tiehca/apitest/heshang/HeshangApplication.java
@@ -0,0 +1,13 @@
+package com.tiehca.apitest.heshang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeshangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeshangApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/tiehca/apitest/heshang/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.tiehca.apitest.heshang;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(HeshangApplication.class);
+    }
+
+}
